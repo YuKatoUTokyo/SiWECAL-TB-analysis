b3aea78d3b6b9ae049063131ac9178d776a2bb95
katou <katou@icepp.s.u-tokyo.ac.jp>
2019-08-09 17:51:26 +0900

    update to match FEV13

--- proto/example.h
+++ proto/example.h
@@ -28,7 +28,7 @@ class example {
 public :
    TTree          *fChain;   //!pointer to the analyzed TTree or TChain
    Int_t           fCurrent; //!current Tree number in a TChain
-   Int_t NSLABS=9;
+   Int_t NSLABS=5;
 // Fixed size dimensions of array or collections stored in the TTree if any.
 
    // Declaration of leaf types
--- proto/run_example.cc
+++ proto/run_example.cc
@@ -4,8 +4,11 @@
 
 void run_example(){
 
-  TString run="run_32002";
-  TString filename = "/home/irles/WorkAreaECAL/2019/TB201906/EvBuilt/"+run+"_build.root";
+  TString run="run_30003";
+  //TString filename = "/home/irles/WorkAreaECAL/2019/TB201906/EvBuilt/"+run+"_build.root";
+  //TString filename = "/home/katou/tb2019/TB2019_06/run_30/"+run+"_build.root";
+  TString filename = "/gluster/maxi/ilc/tb2019/TB2019_06/run_30/"+run+"_build.root";
+  //TString filename = "./output/"+run+"_build.root";
   example ss(filename);
   ss.SimpleDistributionsTrack("_"+run);
   ss.SimpleEvDisplayTrack("_"+run);
